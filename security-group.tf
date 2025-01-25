0dde79bdd5ff60ea57b00e5123bcbafbd49ebc8a
Moyosore Baiye <moyosorebaiye@gmail.com>
2025-01-25 21:54:05 +0100

    completed

--- security-group.tf
+++ security-group.tf
@@ -6,29 +6,29 @@ resource "aws_security_group" "alb_security_group" {
   vpc_id      = aws_vpc.vpc.id
 
   ingress {
-    description      = "http access"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "http access"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    description      = "https access"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "https access"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = -1
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags   = {
+  tags = {
     Name = "${var.project_name}-${var.environment}-alb-sg"
   }
 }
@@ -41,21 +41,21 @@ resource "aws_security_group" "ssh_security_group" {
   vpc_id      = aws_vpc.vpc.id
 
   ingress {
-    description      = "ssh access"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = [var.ssh_location]
+    description = "ssh access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.ssh_location]
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = -1
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags   = {
+  tags = {
     Name = "${var.project_name}-${var.environment}-ssh-sg"
   }
 }
@@ -68,37 +68,37 @@ resource "aws_security_group" "webserver_security_group" {
   vpc_id      = aws_vpc.vpc.id
 
   ingress {
-    description      = "http access"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    security_groups  = [aws_security_group.alb_security_group.id]
+    description     = "http access"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_security_group.id]
   }
 
   ingress {
-    description      = "https access"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    security_groups  = [aws_security_group.alb_security_group.id]
+    description     = "https access"
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_security_group.id]
   }
 
   ingress {
-    description      = "ssh access"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    security_groups  = [aws_security_group.ssh_security_group.id]
+    description     = "ssh access"
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.ssh_security_group.id]
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = -1
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags   = {
+  tags = {
     Name = "${var.project_name}-${var.environment}-webserver-sg"
   }
 }
@@ -111,29 +111,29 @@ resource "aws_security_group" "database_security_group" {
   vpc_id      = aws_vpc.vpc.id
 
   ingress {
-    description      = "mysql access"
-    from_port        = 3306
-    to_port          = 3306
-    protocol         = "tcp"
-    security_groups  = [aws_security_group.webserver_security_group.id]
+    description     = "mysql access"
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.webserver_security_group.id]
   }
 
   ingress {
-    description      = "ssh access"
-    from_port        = 3306
-    to_port          = 3306
-    protocol         = "tcp"
-    security_groups  = [aws_security_group.ssh_security_group.id]
+    description     = "ssh access"
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.ssh_security_group.id]
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = -1
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags   = {
+  tags = {
     Name = "${var.project_name}-${var.environment}-database-sg"
   }
 }
--- variables.tf
+++ variables.tf
@@ -55,5 +55,76 @@ variable "private_data_subnet_az2_cidr_block" {
 variable "ssh_location" {
   description = "The IP address that can be used to SSH to the EC2 instances"
   type        = string
-  
+
+}
+
+# rds variables
+variable "database_snapshot_identifier" {
+  description = "The identifier of the database snapshot to restore the database from"
+  type        = string
+
+}
+
+variable "database_instance_class" {
+  description = "The instance class of the database instance"
+  type        = string
+
+}
+
+variable "database_instance_identifier" {
+  description = "The identifier of the database instance"
+  type        = string
+
+}
+
+# ALB variables
+variable "ssl_certificate_arn" {
+  description = "The ARN of the SSL certificate to use for HTTPS"
+  type        = string
+
+}
+
+# sns variables
+variable "operator_email" {
+  description = "The email address of the operator to receive notifications"
+  type        = string
+
+}
+
+# asg variables
+variable "launch_template_name" {
+  description = "The name of the launch template"
+  type        = string
+  default     = "mobann-launch-template"
+}
+
+variable "ec2_image_id" {
+  description = "The ID of the AMI to use for the EC2 instances"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "The instance type of the EC2 instances"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "ec2_key_name" {
+  description = "The name of the EC2 key pair to use for the EC2 instances"
+  type        = string
+  default     = "nov24"
+}
+
+# route53 variables
+variable "domain_name" {
+  description = "The domain name to use for the Route 53 hosted zone"
+  type        = string
+  default     = "mobanntech.com"
+}
+
+variable "record_name" {
+  description = "sub domain name"
+  type        = string
+  default     = "www"
+
 }
