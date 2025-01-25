0f584bc6bd38dc15bcefb683601eac00dcd15679
Moyosore Baiye <moyosorebaiye@gmail.com>
2025-01-25 20:01:06 +0100

    created VPC files

--- variables.tf
+++ variables.tf
@@ -2,15 +2,51 @@
 
 variable "aws_region" {
   description = "AWS region to create resources in"
-  type = string
+  type        = string
 }
 
 variable "project_name" {
   description = "Name of the project"
-  type = string
+  type        = string
 }
 
 variable "environment" {
   description = "Environment to deploy resources to"
-  type = string
+  type        = string
+}
+
+# VPC variables
+variable "vpc_cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnet_az1_cidr_block" {
+  description = "CIDR block for the public subnet in availability zone 1"
+  type        = string
+}
+
+variable "public_subnet_az2_cidr_block" {
+  description = "CIDR block for the public subnet in availability zone 2"
+  type        = string
+}
+
+variable "private_app_subnet_az1_cidr_block" {
+  description = "CIDR block for the private app subnet in availability zone 1"
+  type        = string
+}
+
+variable "private_app_subnet_az2_cidr_block" {
+  description = "CIDR block for the private app subnet in availability zone 2"
+  type        = string
+}
+
+variable "private_data_subnet_az1_cidr_block" {
+  description = "CIDR block for the private data subnet in availability zone 1"
+  type        = string
+}
+
+variable "private_data_subnet_az2_cidr_block" {
+  description = "CIDR block for the private data subnet in availability zone 2"
+  type        = string
 }
